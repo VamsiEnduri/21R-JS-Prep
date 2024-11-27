cc62ed7446950ca8c7f9af240348230d72fb2ff7
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-11-27 16:35:43 +0530

    interviewPrep1

---
+++ index.js
@@ -0,0 +1,15 @@
+// let a=null
+// console.log(typeof a);
+// window.location();
+
+// console.log(a);
+// var a=10;
+
+
+// function z(){
+//     var a="100"
+//     let b=200;
+//     const c=300;
+
+// }
+// z()
